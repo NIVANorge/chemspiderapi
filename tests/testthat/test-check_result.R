2a692ef80565948b16e79bf028682271157e5e4e
Raoul Wolf <Raoul.Wolf@niva.no>
2019-08-13 15:03:33 +0200

    Added test for check_result()

---
+++ tests/testthat/test-check_result.R
@@ -0,0 +1,16 @@
+library(chemspiderapi)
+
+context("check_result")
+
+test_that("check_result() fails if no result is provided.", {
+  expect_error(
+    check_result()
+    )
+})
+
+test_that("check_result() fails if no result is provided.", {
+  expect_length(
+    check_result(result = data.frame(column_1 = 1:10, stringsAsFactors = FALSE)), 
+    10
+  )
+})
